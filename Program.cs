f6b2a8f17dd85ea4788536523ffe4ad32b191f56
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-04-10 17:17:31 -0300

    operators

--- Program.cs
+++ Program.cs
@@ -7,53 +7,29 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            bool completo = false;
-            char genero = 'A';
-            char unicode = '\u03b2';
-            sbyte x = 127;
-            byte n1 = 126;
-            int n2 = 1000;
-            int n3 = 2147483647;
-            long n4 = 2147483648L;
-            float n5 = 4.5f;
-            double n6 = 4.5;
-            string nome = "Vinicius Quirino"; //IMUTÁVEL
-            object obj1 = "Eva"; //GENÉRICOH
-            object obj2 = 4.7f;
+            int a = 10;
+            Console.WriteLine(a);
+            a += 2;
+            Console.WriteLine(a);
+            a -= 2;
+            Console.WriteLine(a);
+            a *= 2;
+            Console.WriteLine(a);
+            a /= 4;
+            Console.WriteLine(a);
+            int b = 100;
+            b %= 3; //RESTO DA DIVISÃO
+            Console.WriteLine(b);
 
-            int anos = 23;
+            string letras = "ABC";
+            System.Console.WriteLine(letras += "DEF");
 
-            double saldo = 10.35785;
+            int number = 1;
+            number++;
+            System.Console.WriteLine(number);
 
-            x++;
-            Console.WriteLine(completo);
-            Console.WriteLine(genero);
-            Console.WriteLine(unicode);
-            Console.WriteLine(x);
-            Console.WriteLine(n1);
-            Console.WriteLine(n2);
-            Console.Write(n3);
-            Console.Write(n4);
-            Console.WriteLine(n5);
-            Console.WriteLine(n6);
-            Console.WriteLine(nome);
-            Console.WriteLine(obj1);
-            Console.WriteLine(obj2);
-            Console.WriteLine(int.MinValue);
-            Console.WriteLine(double.MinValue);
-            Console.WriteLine(long.MaxValue);
-            Console.WriteLine(decimal.MaxValue);
-            Console.WriteLine(saldo.ToString("F2")); //QUANTIDADE DE CASAS DECIMAIS
-            Console.WriteLine(saldo.ToString("F5", CultureInfo.InvariantCulture)); //FORMATA COM .
-
-            //PLACEHOLDERS
-            Console.WriteLine("{0} tem {1} anos e saldo igual a {2:F2} reais.", nome, anos, saldo);
-
-            //INTERPOLAÇÃO
-            System.Console.WriteLine($"{nome} tem {anos} anos e saldo igual a {saldo:F2} reais.");
-
-            //CONCATENAÇÃO
-            Console.WriteLine(nome + " tem " + anos + " anos e saldo igual a " + saldo.ToString("F2") + " reais.");
+            number--;
+            System.Console.WriteLine(number);
         }
     }
 }
