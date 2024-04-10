e45bfb2c504b41c074daa768d4135eb36d13f9ba
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-04-10 15:39:56 -0300

    data types

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            sbyte x = 127;
+            byte n1 = 126;
+            int n2 = 1000;
+            int n3 = 2147483647;
+            long n4 = 2147483648L;
+
+            x++;
+            Console.WriteLine(completo);
+            Console.WriteLine(x);
+            Console.WriteLine(n1);
+            Console.WriteLine(n2);
+            Console.WriteLine(n3);
+            Console.WriteLine(n4);
+            
+        }
+    }
+}
