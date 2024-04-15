e4c3550ba42c9465eee86f388b2b9b0d716012c4
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-04-15 15:49:24 -0300

    dataInput

--- Program.cs
+++ Program.cs
@@ -7,30 +7,24 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            // ENTRADA DE DADOS
-            // string frase = Console.ReadLine();
-            // System.Console.WriteLine(frase);
+           int a = 10;
+           bool c1 = a < 10;
+           bool c2 = a < 20;
+           bool c3 = a > 10;
+           bool c4 = a > 5;
+           bool c5 = a == 10;
+           bool c6 = a <= 10;
+           bool c7 = a >= 10;
+           bool c8 = a != 10;
 
-        
-            // string[] vet = Console.ReadLine().Split(' ');
-            // System.Console.WriteLine(vet[0]);
-            // System.Console.WriteLine(vet[1]);
-
-            // int n1 = int.Parse(Console.ReadLine());
-            // char ch = char.Parse(Console.ReadLine()); //1 caracter
-            // double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            // System.Console.WriteLine(n2.ToString("F2"));
-
-            string [] arr = Console.ReadLine().Split();
-            string nome = arr[0];
-            int idade = int.Parse(arr[1]);
-            char sexo = char.Parse(arr[2]);
-            double altura = double.Parse(arr[3]);
-
-            string resultado = $"{nome} {idade} {sexo}";
-            resultado.Replace("%", "");
-
-            System.Console.WriteLine(resultado);
+           System.Console.WriteLine(c1);
+           System.Console.WriteLine(c2);
+           System.Console.WriteLine(c3);
+           System.Console.WriteLine(c4);
+           System.Console.WriteLine(c5);
+           System.Console.WriteLine(c6);
+           System.Console.WriteLine(c7);
+           System.Console.WriteLine(c8);
         }
     }
 }
