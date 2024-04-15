edde82c551758e2cc7bbbc8b772838627384b3e3
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-04-15 15:59:15 -0300

    logicalOperators

--- Program.cs
+++ Program.cs
@@ -7,24 +7,16 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-           int a = 10;
-           bool c1 = a < 10;
-           bool c2 = a < 20;
-           bool c3 = a > 10;
-           bool c4 = a > 5;
-           bool c5 = a == 10;
-           bool c6 = a <= 10;
-           bool c7 = a >= 10;
-           bool c8 = a != 10;
-
-           System.Console.WriteLine(c1);
-           System.Console.WriteLine(c2);
-           System.Console.WriteLine(c3);
-           System.Console.WriteLine(c4);
-           System.Console.WriteLine(c5);
-           System.Console.WriteLine(c6);
-           System.Console.WriteLine(c7);
-           System.Console.WriteLine(c8);
+          bool c1 = 2 > 3 && 4 != 5;
+          bool c2 = 2 <= 3 && 4 != 5;
+          bool c3 = 2 > 3 || 4 != 5;
+          bool c4 = !(2 > 3) && 4 != 5;
+          bool c5 = 2 > 3 && 4 == 5;
+          System.Console.WriteLine(c1);
+          System.Console.WriteLine(c2);
+          System.Console.WriteLine(c3);
+          System.Console.WriteLine(c4);
+          System.Console.WriteLine(c5);
         }
     }
 }
